ac139ace8f63d3366f0345718b6032ab490c2792
mostafa <moa10266@gmail.com>
2026-03-04 01:38:53 +0200

    creat the booking body

--- lib/features/player/profile/views/profile_view.dart
+++ lib/features/player/profile/views/profile_view.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:mala3bna/core/widgets/custom_text.dart';
+// import 'package:mala3bna/core/widgets/custom_text.dart';
+import 'package:mala3bna/features/player/profile/views/my_bookings.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(child: customText(text: 'Profile Screen', size: 30)),
-    );
+    return const MyBookings();
   }
 }
---
+++ lib/features/player/profile/views/widgets/my_booking_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:mala3bna/features/player/profile/views/widgets/custome_app_bar.dart';
+
+class MyBookingBody extends StatelessWidget {
+  const MyBookingBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 48, left: 8, right: 8),
+      child: Column(
+        children: [
+          CustomeAppBar(
+            title: "My Bookings",
+            leadingIcon: Icon(Icons.arrow_back),
+            onPressedLeadingIcon: () {},
+            onPressedTrailingIcon: () {},
+            trailainIcone: Icon(Icons.more_vert),
+          ),
+          const Gap(24),
+          
+
+        ],
+      ),
+    );
+  }
+}
