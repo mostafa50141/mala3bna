22a98589cf0a490210c5ddd4e33b6d3e8aa0102b
mostafa <moa10266@gmail.com>
2026-03-12 22:14:44 +0200

    add post fun to the api server

--- lib/core/utils/api_server.dart
+++ lib/core/utils/api_server.dart
@@ -1,12 +1,20 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final String baseUrl = "https://www.googleapis.com/books/v1";
+  final String _baseUrl = "https://bqsl6hrg-8000.uks1.devtunnels.ms/api/v1/";
   final Dio dio;
 
   ApiService({required this.dio});
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await dio.get('$baseUrl$endPoint');
+    var response = await dio.get('$_baseUrl$endPoint');
+    return response.data;
+  }
+
+  Future<Map<String, dynamic>> post({
+    required String endPoint,
+    required Map<String, dynamic> body,
+  }) async {
+    var response = await dio.post('$_baseUrl$endPoint', data: body);
     return response.data;
   }
 }
