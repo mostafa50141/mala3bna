f8e138fe7f4107aa2a744d17824eb1f1517396bd
mostafa <moa10266@gmail.com>
2026-03-13 19:20:47 +0200

    create the server locator injection

--- lib/core/utils/server_locator.dart
+++ lib/core/utils/server_locator.dart
@@ -1,13 +1,13 @@
-// import 'package:dio/dio.dart';
-// import 'package:get_it/get_it.dart';
-// import 'package:mala3bna/core/utils/api_server.dart';
-// import 'package:mala3bna/features/auth/data/Repos/auth_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mala3bna/core/utils/api_server.dart';
+import 'package:mala3bna/features/auth/data/Repos/auth_repo_imp.dart';
 
-// final getIt = GetIt.instance;
+final getIt = GetIt.instance;
 
-// void setup() {
-//   getIt.registerSingleton<ApiService>(ApiService(dio: Dio()));
-//   getIt.registerSingleton<AuthRepoImp>(
-//     AuthRepoImp(apiService: getIt.get<ApiService>()),
-//   );
-// }
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(dio: Dio()));
+  getIt.registerSingleton<AuthRepoImp>(
+    AuthRepoImp(apiService: getIt.get<ApiService>()),
+  );
+}
