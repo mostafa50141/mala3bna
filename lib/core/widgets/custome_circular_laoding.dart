ba18f5e549ca257a906e5bd947f1933f4e27631c
mostafa <moa10266@gmail.com>
2026-04-03 17:33:17 +0200

    create cutome circuler laoding

--- lib/core/widgets/custome_circular_laoding.dart
+++ lib/core/widgets/custome_circular_laoding.dart
@@ -1,17 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:mala3bna/core/constants/app_colors.dart';
 
-class CustomeCircularLaoding extends StatelessWidget {
+// CustomeCircularLaoding
+class CustomeCircularLaoding extends StatefulWidget {
   const CustomeCircularLaoding({super.key});
+  @override
+  State<CustomeCircularLaoding> createState() => _CustomeCircularLaodingState();
+}
+
+class _CustomeCircularLaodingState extends State<CustomeCircularLaoding>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 30,
-      height: 30,
-      child: Lottie.asset(
-        'assets/animations/loading_indicator.json',
-        fit: BoxFit.contain,
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (_, __) => SizedBox(
+        width: 64,
+        height: 64,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            // Outer ring
+            Transform.rotate(
+              angle: _controller.value * 2 * 3.14159,
+              child: Container(
+                width: 64,
+                height: 64,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: AppColors.primaryColor, width: 2.5),
+                ),
+              ),
+            ),
+            Transform.rotate(
+              angle: -_controller.value * 2 * 3.14159,
+              child: Container(
+                width: 44,
+                height: 44,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.primaryColor.withOpacity(0.5),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: AppColors.primaryColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
